762688b52ea9798fe42303ee6317f44e2644a143
Marcus Hansson <90m.hansson@gmail.com>
2017-09-30 14:49:25 +0000

    Finished the first 4 tasks and started pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,17 @@
 #write your code here
+def translate word
+  pig_word = ""
+  if is_vowel(word[0...1])
+    pig_word = "#{word}ay"
+  else
+    #do stuff
+  end
+  pig_word
+end
+
+def is_vowel chr
+  if chr == "a" || chr == "e" || chr == "i" || chr == "j" || chr == "o" || chr == "u"
+    return true
+  end
+  return false
+end
